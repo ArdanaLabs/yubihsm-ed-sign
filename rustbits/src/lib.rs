255c3dbfa4d264cffb0713c98de63a1264e456dd
Michael Litchard <michael.litchard@platonic.systems>
2022-05-02 15:19:01 +0000

    current state broken

--- rustbits/src/lib.rs
+++ rustbits/src/lib.rs
@@ -1,5 +1,7 @@
 use yubihsm::client::Error;
 use yubihsm::{object, Capability, Client, Connector, Domain};
+use yubihsm::object::Id;
+use yubihsm::{asymmetric::signature::Signer as _};
 use yubihsm::asymmetric::Algorithm;
 use yubihsm::ed25519::Signature;
 use std::slice;
@@ -10,19 +12,46 @@ const SIGNATURE_SIZE: usize = 64;
 
 mod test;
 
-pub fn create_client() -> Result<Client, Error> {
-  let connector: Connector = Connector::usb(&Default::default());
+pub fn create_client(connector: Connector) -> Result<Client, Error> {
+ // let connector: Connector = Connector::usb(&Default::default());
   Client::open(connector, Default::default(), true)
 }
 
+
 #[no_mangle]
-pub extern fn put_ed_key(id: u16, label: &[u8; LABEL_SIZE], domains: u16, key: &[u8; KEY_SIZE]) -> () {
-  let client: Client = create_client().expect("could not connect to YubiHSM");
+pub extern fn sign_with_ed_key(id: u16, msgptr: *const u8, msglen: usize, result: *mut u8) -> () {
+  let connector: Connector = Connector::usb(&Default::default());
+  //let _: Connector = mockhsm();
+  let client: Client = create_client(connector).expect("could not connect to YubiHSM");
+  let msg: &[u8] = unsafe { slice::from_raw_parts(msgptr, msglen) };
+  sign_with_ed_key_internal(client,id,msg,result);
+
+}
+
+pub fn sign_with_ed_key_internal(client: Client,id: u16, msg: &[u8], result: *mut u8) -> () {
+ // let client: Client = create_client().expect("could not connect to YubiHSM");
+  let sig: Signature = client.sign_ed25519(id, msg).expect("could not get the signature");
+  let sigbytes: [u8; SIGNATURE_SIZE] = sig.to_bytes();
+ // return sigbytes;
+  unsafe { sigbytes.as_ptr().copy_to(result, SIGNATURE_SIZE) }
+}
+
+#[no_mangle]
+pub extern fn put_ed_key(id: u16, label: &[u8; LABEL_SIZE], domain: u16, key: &[u8; KEY_SIZE]) -> () {
+ // let _ = Domain[0];
+  let connector: Connector = Connector::usb(&Default::default());
+  let client: Client = create_client(connector).expect("could not connect to YubiHSM");
+  put_ed_key_internal(&client,id,label,domain,key);
+}
+
+pub fn put_ed_key_internal(client: &Client, id: u16, label: &[u8; LABEL_SIZE], domain: u16, key: &[u8; KEY_SIZE]) -> () {
   let _ = client.delete_object(id, object::Type::AsymmetricKey);
+  let t_domain: Domain =  Domain::from_bits_truncate(domain);
+ // print!("domain is {}", tDomain);
   client.put_asymmetric_key(
     id,
     object::Label::from_bytes(label).expect("failed to construct Label from byte array"),
-    Domain::from_bits_truncate(domains),
+    t_domain,
     Capability::SIGN_EDDSA,
     Algorithm::Ed25519,
     &key[..]
@@ -30,11 +59,14 @@ pub extern fn put_ed_key(id: u16, label: &[u8; LABEL_SIZE], domains: u16, key: &
   ()
 }
 
-#[no_mangle]
-pub extern fn sign_with_ed_key(id: u16, msgptr: *const u8, msglen: usize, result: *mut u8) -> () {
-  let msg: &[u8] = unsafe { slice::from_raw_parts(msgptr, msglen) };
-  let client: Client = create_client().expect("could not connect to YubiHSM");
-  let sig: Signature = client.sign_ed25519(id, msg).expect("could not get the signature");
-  let sigbytes: [u8; SIGNATURE_SIZE] = sig.to_bytes();
-  unsafe { sigbytes.as_ptr().copy_to(result, SIGNATURE_SIZE) }
-}
+
+fn make_asymmetric_key(label: &[u8; LABEL_SIZE],domain: Domain, key_id: u16) -> Result<object::Id,Error>{
+  let connector: Connector = Connector::usb(&Default::default());
+  let client: Client = create_client(connector).expect("could not connect to YubiHSM");
+  return client.generate_asymmetric_key(
+    key_id,
+    object::Label::from_bytes(label).expect("failed to construct Label from byte array"),
+    domain,
+    Capability::SIGN_EDDSA,
+    Algorithm::Ed25519 );
+ }
--- rustbits/src/test.rs
+++ rustbits/src/test.rs
@@ -1,19 +1,103 @@
+
 #[cfg(test)]
 mod tests {
+  use yubihsm::{object, Capability, Client, Connector, Domain};
+  use yubihsm::{asymmetric::signature::Signer as _, ed25519};
+  
+  extern crate libc;
+  use core::mem;
   use crate::create_client;
+  use crate::sign_with_ed_key_internal;
+  const TEST_KEY_ID: u16 = 200;
+  const TEST_SIGNING_KEY_ID: yubihsm::object::Id = 200;
+  const TEST_SIGNING_KEY_DOMAINS: yubihsm::Domain = yubihsm::Domain::DOM1;
+  const TEST_SIGNING_KEY_CAPABILITIES: yubihsm::Capability = yubihsm::Capability::SIGN_EDDSA;
+  const TEST_MESSAGE: &[u8] =
+    b"The Edwards-curve Digital Signature yubihsm::asymmetric::Algorithm  (EdDSA) is a \
+        variant of Schnorr's signature system with (possibly twisted) Edwards curves.";
+  const TEST_DOMAIN: u16 = 0x0001;
+  const LABEL_SIZE: usize = 40;
+  const TEST_SIGNING_KEY_LABEL: &str = "Signatory test key";
+  const MESSAGE: &[u8] = b"";
+  const SecretKey: &[u8; 32] = b"\x9D\x61\xB1\x9D\xEF\xFD\x5A\x60\xBA\x84\x4A\xF4\x92\xEC\x2C\xC4\x44\x49\xC5\x69\x7B\x32\x69\x19\x70\x3B\xAC\x03\x1C\xAE\x7F\x60";
+  const PublicKey: &[u8; 32] = b"\xD7\x5A\x98\x01\x82\xB1\x0A\xB7\xD5\x4B\xFE\xD3\xC9\x64\x07\x3A\x0E\xE1\x72\xF3\xDA\xA6\x23\x25\xAF\x02\x1A\x68\xF7\x07\x51\x1A";
+  
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
-    #[test]
-    fn test_create_client() {
-      let res = create_client();
-      let isgood = match res {
-        Ok(_) => true,
-        Err(_) => false,
-      };
+ //   #[test]
+ //   fn test_create_client() {
+ //    let connector: Connector = Connector::usb(&Default::default());
+     // let connector: Connector = Connector::mockhsm;
+ //    let client = create_client(connector).expect("fuck");
+      
+ //    let isgood = match client {
+  //      Ok(_) => true,
+  //      Err(_) => false,
+   //   };
     
-      assert_eq!(isgood,true);
+ //     assert_eq!(true,true);
+      
+ //   }
+    
+    #[test]
+    fn test_sign_with_ed_key() {
+      use yubihsm::{asymmetric,Domain};
+    
+      use crate::Signature;
+      let mut placeholder: u8 = 0x1Au8;
+      let res = &mut placeholder as *mut u8; 
+      
+      let connector: Connector = Connector::usb(&Default::default());
+      let label = b"\x73\x74\x75\x70\x69\x64\x73\x74\x75\x70\x69\x64\x73\x74\x75\x70\x69\x64\x73\x74\x75\x70\x69\x64\x73\x74\x75\x70\x69\x64\x73\x74\x75\x70\x69\x64\x73\x74\x75\x70";
+      
+      let client: Client = create_client(connector).expect("could not connect to YubiHSM");
+      client.generate_asymmetric_key(
+        TEST_KEY_ID,
+        TEST_SIGNING_KEY_LABEL.into(),
+        Domain::DOM1,
+        TEST_SIGNING_KEY_CAPABILITIES,
+        asymmetric::Algorithm::Ed25519
+      ).unwrap();
+
+
+      
+      
+      sign_with_ed_key_internal(client,TEST_KEY_ID, MESSAGE,res);
+      let usafe = unsafe {*res};
+      
+     // let signature: Signature = res as Signature;
+      print!("res {}", usafe);
+//      let sig = sign_with_ed_key_internal(client,TEST_KEY_ID, Message);
+     // client.
+   //  unsafe {libc::free(res as *mut libc::c_void)};
+   
+
     }
 
-}
+    fn create_yubihsm_key(client: &Client) {
+      // Delete the key in TEST_KEY_ID slot it exists
+      // Ignore errors since the object may not exist yet
+      let _ = client.delete_object(TEST_SIGNING_KEY_ID, yubihsm::object::Type::AsymmetricKey);
+    
+      // Create a new key for testing
+      client
+          .generate_asymmetric_key(
+              TEST_SIGNING_KEY_ID,
+              TEST_SIGNING_KEY_LABEL.into(),
+              TEST_SIGNING_KEY_DOMAINS,
+              TEST_SIGNING_KEY_CAPABILITIES,
+              yubihsm::asymmetric::Algorithm::Ed25519,
+          )
+          .unwrap();
+    }
+  }
+
+
+
+
+
+
+
+
