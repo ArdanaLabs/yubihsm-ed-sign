dd498bf2c92eb5f93d181cefc422448c367c5051
Sridhar Ratnakumar <srid@srid.ca>
2022-05-04 14:37:59 -0400

    nix: Redo Rust nix setup to provide dev env

--- flake.nix
+++ flake.nix
@@ -1,37 +1,76 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    dream2nix = {
-      url = "github:davhau/dream2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     flake-compat.url = "github:edolstra/flake-compat";
     flake-compat.flake = false;
     flake-compat.inputs.nixpkgs.follows = "nixpkgs";
+
+    # Rust inputs
+    crate2nix.url = "github:kolloch/crate2nix";
+    crate2nix.flake = false;
+    rust-overlay.url = "github:oxalica/rust-overlay";
   };
 
-  outputs = { self, nixpkgs, dream2nix, ... }@inputs:
+  outputs = { self, nixpkgs, ... }@inputs:
     let
+      name = "yubihsmedsign";
       version = builtins.substring 0 8 self.lastModifiedDate;
       supportedSystems = [ "x86_64-linux" ];
+      # Rust release channel to use.
+      # https://rust-lang.github.io/rustup/concepts/channels.html
+      rustChannel = "stable";
       forSystems = systems: f:
         nixpkgs.lib.genAttrs systems
         (system: f system nixpkgs.legacyPackages.${system});
       forAllSystems = forSystems supportedSystems;
-      nixpkgsFor = forAllSystems (system: pkgs: import nixpkgs { inherit system; overlays = [ ]; });
-      dream2nix = inputs.dream2nix.lib2.init {
-        systems = supportedSystems;
-        config = {
-          projectRoot = ./.;
-          overridesDirs = [ "${inputs.dream2nix}/overrides" ];
-        };
-      };
-      rustPackage = system: 
-        (dream2nix.makeFlakeOutputs {
-            source = ./rustbits;
-          }).packages.${system}.yubihsm-ed-sign;
-      haskellProject = returnShellEnv: forAllSystems (system: pkgs: 
+      nixpkgsFor = forAllSystems (system: _pkgs: 
+        import nixpkgs { 
+          inherit system; 
+          overlays = [ 
+            inputs.rust-overlay.overlay
+            (self: super: {
+              # Because rust-overlay bundles multiple rust packages into one
+              # derivation, specify that mega-bundle here, so that crate2nix
+              # will use them automatically.
+              rustc = self.rust-bin.${rustChannel}.latest.default;
+              cargo = self.rust-bin.${rustChannel}.latest.default;
+            })
+          ]; 
+        }
+      );
+
+      # Create the cargo2nix project
+      rustProject = system: 
         let 
+          pkgs = nixpkgsFor.${system};
+          # This is provides Haskell's `callCabal2Nix` but for Rust.
+          # cf. https://github.com/kolloch/crate2nix/issues/110
+          inherit (import "${inputs.crate2nix}/tools.nix" { inherit pkgs; })
+            generatedCargoNix;
+        in pkgs.callPackage
+            (generatedCargoNix {
+              inherit name;
+              src = ./rustbits;
+            })
+            {
+              buildRustCrate = null; # https://github.com/kolloch/crate2nix/pull/178#issuecomment-820692187
+
+              # Individual crate overrides go here
+              # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
+              defaultCrateOverrides = pkgs.defaultCrateOverrides // {
+                # The project crate itself is overriden here. Typically we
+                # configure non-Rust dependencies (see below) here.
+                ${name} = oldAttrs: rustDeps system;
+              };
+            };
+      rustDeps = system: {
+        # Configuration for the non-Rust dependencies
+        buildInputs = with nixpkgsFor.${system}; [ openssl.dev ];
+        nativeBuildInputs = with nixpkgsFor.${system}; [ rustc cargo pkgconfig ];
+      };
+      haskellProject = system: returnShellEnv: 
+        let 
+          pkgs = nixpkgsFor.${system};
           # Change GHC version here.
           hp = pkgs.haskellPackages;
         in
@@ -63,14 +102,28 @@
                   pkgs.cargo
                   pkgs.rustc
                 ]);
-          }
-        );
+          };
     in
     {
-      devShell = haskellProject true;
+      devShell = forAllSystems (system: _pkgs:
+        let pkgs = nixpkgsFor.${system};
+        in (haskellProject system true).overrideAttrs (oa: {
+          inputsFrom = (oa.inputsFrom or []) ++ [self.packages.${system}.yubihsm-ed-sign];
+          buildInputs = oa.buildInputs ++ (rustDeps system).buildInputs ++ (with pkgs;
+            # Tools you need for development go here.
+            [
+              nixpkgs-fmt
+              cargo-watch
+              pkgs.rust-bin.${rustChannel}.latest.rust-analysis
+              pkgs.rust-bin.${rustChannel}.latest.rls
+            ]);
+          RUST_SRC_PATH = "${pkgs.rust-bin.${rustChannel}.latest.rust-src}/lib/rustlib/src/rust/library";
+        })
+      );
 
       packages = forAllSystems (system: _pkgs: {
-        yubihsm-ed-sign = rustPackage system; 
+        yubihsm-ed-sign = (rustProject system).rootCrate.build.lib; 
+        default = haskellProject system false;
       });
     };
 }
