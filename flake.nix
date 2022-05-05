ae86ac015cf686b01a0b9bb25fd7b509615423c9
Sridhar Ratnakumar <srid@srid.ca>
2022-05-05 10:17:13 -0400

    nix: Add comments

--- flake.nix
+++ flake.nix
@@ -2,7 +2,11 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Flake utils and compat
+    # - flake-utils provides `eachDefaultSystem` which simplifies our flake.
+    #   See the `outputsFor` function, which factors out the 'system' at
+    #   top-level (as function argument)
+    # - flake-compat allows us to provide a shell.nix until
+    #   https://github.com/arrterian/nix-env-selector/issues/53 is fixed.
     flake-utils.url = "github:numtide/flake-utils";
     flake-utils.inputs.nixpkgs.follows = "nixpkgs";
     flake-compat.url = "github:edolstra/flake-compat";
@@ -10,6 +14,10 @@
     flake-compat.inputs.nixpkgs.follows = "nixpkgs";
 
     # Rust inputs
+    # - crate2nix provides developer environments, unlike the Rust support in nixpkgs.
+    # - rust-overlay provides the suite of Rust toolchains we need for IDE
+    #   support, etc. It is also fairly easy to switch between different Rust
+    #   channels.
     crate2nix.url = "github:kolloch/crate2nix";
     crate2nix.flake = false;
     rust-overlay.url = "github:oxalica/rust-overlay";
@@ -17,7 +25,10 @@
 
   outputs = { self, nixpkgs, ... }@inputs:
     let
-      # Function that produces Flake outputs for the given system.
+      # A function that produces Flake outputs for the given system.
+      #
+      # Define the entire flake inside this function, which factors out the
+      # 'system' at top-level var.
       outputsFor = system:
         let
           name = "yubihsmedsign";
@@ -46,14 +57,9 @@
               inherit (import "${inputs.crate2nix}/tools.nix" { inherit pkgs; })
                 generatedCargoNix;
             in
-            pkgs.callPackage
-              (generatedCargoNix {
-                inherit name;
-                src = ./rustbits;
-              })
+            pkgs.callPackage (generatedCargoNix { inherit name; src = ./rustbits; })
               {
                 buildRustCrate = null; # https://github.com/kolloch/crate2nix/pull/178#issuecomment-820692187
-
                 # Individual crate overrides go here
                 # Example: https://github.com/balsoft/simple-osd-daemons/blob/6f85144934c0c1382c7a4d3a2bbb80106776e270/flake.nix#L28-L50
                 defaultCrateOverrides = pkgs.defaultCrateOverrides // {
