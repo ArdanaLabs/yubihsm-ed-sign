07e2a4f2e209113e2ec88538f849e3341983e865
Sridhar Ratnakumar <srid@srid.ca>
2022-05-05 10:55:23 -0400

    nix: Move mergeDevShells to separate file

--- flake.nix
+++ flake.nix
@@ -36,30 +36,7 @@
             inherit system;
             overlays = rustOverlays;
           };
-
-          # Merge multiple devShells into one, such that entering the resulting
-          # dev shell puts one in the environment of all the input dev shells.
-          mergeDevShells = envs: pkgs.mkShell (builtins.foldl'
-            (a: b:
-              # Standard shell attributes
-              {
-                buildInputs = a.buildInputs ++ b.buildInputs;
-                nativeBuildInputs = a.nativeBuildInputs ++ b.nativeBuildInputs;
-                propagatedBuildInputs = a.propagatedBuildInputs ++ b.propagatedBuildInputs;
-                propagatedNativeBuildInputs = a.propagatedNativeBuildInputs ++ b.propagatedNativeBuildInputs;
-                shellHook = a.shellHook + "\n" + b.shellHook;
-              } //
-              # Environment variables
-              (
-                let
-                  isUpperCase = s: pkgs.lib.strings.toUpper s == s;
-                  filterUpperCaseAttrs = attrs: pkgs.lib.attrsets.filterAttrs (n: _: isUpperCase n) attrs;
-                in
-                filterUpperCaseAttrs a // filterUpperCaseAttrs b
-              )
-            )
-            (pkgs.mkShell { })
-            envs);
+          mergeDevShells = import ./nix/mergeDevShells.nix { inherit pkgs; };
 
           # Nix for Rust development environment and build
           rustProject =
