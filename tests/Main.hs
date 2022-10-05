c4a7c4a44f3104f0cb688d9bc90d1c6c694510b4
Geometer1729 <16kuhnb@gmail.com>
2022-10-05 11:14:49 -0400

    Revert "make tests fail to see if they are already in CI"
    This reverts commit 2c3a325ca6d44e51e699aa8fc0d223dae77e8da7.

--- tests/Main.hs
+++ tests/Main.hs
@@ -13,15 +13,12 @@ import Lib
       putEdKey,
       signWithEdKey, getPubKey, publicKey )
 import Data.Word (Word16)
-import System.Exit (die)
-import Control.Monad (void)
 
 testKeyID :: Word16
 testKeyID = 200;
 
 main :: IO ()
 main = do
-    void $ die "test failure"
     _ <- runTestTT tests
     return ()
 
